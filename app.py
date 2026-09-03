d5294b298a54478f125c2aa65921f6221783bbf0
123sohails <sshaikh46597@gmail.com>
2026-09-03 17:56:01 +0530

    Add CORS support

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import tensorflow as tf
 from PIL import Image
 import numpy as np
 import os
 
 app = Flask(__name__)
+CORS(app)
 
 # -----------------------------
 # Configuration
